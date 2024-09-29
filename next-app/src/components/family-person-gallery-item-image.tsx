00f566e02c83b30561d1dac63eeeba6b38dc96fe
Sergey Indenbom <dzorogh@gmail.com>
2024-09-29 18:27:30 +0300

    fix: list of people on photos

--- next-app/src/components/family-person-gallery-item-image.tsx
+++ next-app/src/components/family-person-gallery-item-image.tsx
@@ -15,7 +15,7 @@ export const FamilyPersonGalleryItemImage = ({photo}: { photo: Photo }) => {
     return (
         <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger className="cursor-zoom-in group rounded-md overflow-hidden ">
-                <Image src={photo.media_url} width={600} height={600} alt={photo.description} className="rounded-md group-hover:scale-110 transition-transform"/>
+                <Image src={photo.media_url} fill={true} alt={photo.description} className="rounded-md group-hover:scale-110 transition-transform object-contain"/>
             </DialogTrigger>
             <DialogContent onClick={() => setOpen(false)}
                            className="cursor-zoom-out p-0 inline-flex border-0 md:max-w-[calc(100vw-6rem)] max-w-[calc(100vw-3rem)] md:max-h-[calc(100vh-6rem)] max-h-[calc(100vh-3rem)] object-cover overflow-hidden">
--- next-app/src/components/family-person-gallery-item.tsx
+++ next-app/src/components/family-person-gallery-item.tsx
@@ -1,33 +1,56 @@
 import {Photo} from "@/types";
 import {Article} from "@/components/article";
-import {Calendar03Icon, Location01Icon, QuestionIcon} from "hugeicons-react";
+import {Calendar03Icon, Location01Icon} from "hugeicons-react";
 import {FamilyPersonGalleryItemImage} from "@/components/family-person-gallery-item-image";
+import {Badge} from "@/components/ui/badge";
+import Link from "next/link";
 
 export const FamilyPersonGalleryItem = ({photo}: { photo: Photo }) => {
 
     return (
-        <div className="flex flex-col bg-white overflow-hidden break-words rounded-md shadow-lg">
+        <div className="flex h-full flex-col bg-white overflow-hidden break-words rounded-md shadow-lg">
 
-            <FamilyPersonGalleryItemImage photo={photo}/>
+            <div className="aspect-[4/3] overflow-hidden flex items-center justify-center bg-slate-200 relative">
+                <FamilyPersonGalleryItemImage photo={photo}/>
+            </div>
 
-            {photo.description || photo.approximate_date || photo.place ?
-                <div className="p-4 flex flex-col gap-4">
-                    {photo.description ? <div>
+            <div className="p-4 flex flex-col gap-4 grow">
+                <div className="grow">
+                    {photo.description ?
                         <Article content={photo.description}/>
-                    </div> : ""}
+                        : <div className="text-slate-300">Без описания</div>}
+                </div>
 
-                    <div className="flex justify-between items-center">
-                        <div className="text-slate-400 flex items-center gap-2">
-                            <Calendar03Icon/> {photo.approximate_date ?? "?"}
+                {photo.people ?
+                    <div className="text-slate-500">
+                        На фото:
+                        <div className="flex flex-col">
+                            {photo.people.map(person =>
+                                <div className="text-xs">
+                                    <Link className="underline" href={`/person/${person.id}`}>
+                                        {person.full_name}
+                                    </Link>
+                                    {person.position_on_photo ?
+                                        <span className="text-slate-300"> {person.position_on_photo}</span>
+                                        : ""
+                                    }
+                                </div>
+                            )}
                         </div>
+                    </div> : ""
+                }
 
-                        <div className="text-slate-400 flex items-center gap-2">
-                            <Location01Icon/> {photo.place ?? "?"}
-                        </div>
+                <div className="flex justify-between items-center">
+                    <div className="text-slate-400 flex items-center gap-2">
+                        <Calendar03Icon/> {photo.approximate_date ?? "?"}
                     </div>
 
-                </div> : ""
-            }
+                    <div className="text-slate-400 flex items-center gap-2">
+                        <Location01Icon/> {photo.place ?? "?"}
+                    </div>
+                </div>
+
+            </div>
 
         </div>
     )
